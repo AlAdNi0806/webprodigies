5d7de48c48230912d55d9eaa528648dca37237e7
Alex <hetdro451@gmail.com>
2024-01-07 15:41:43 +0300

    first part of homepage

--- dito/server/server-reference-manifest.js
+++ dito/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"KWZeErr5Ne1lxhR8gYWRRKwsxZODZvd+MIqhNuEAAls=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"/RPnoO8n8B+VNkjOfhSX2ITaJwnnoMPwWqyjnQabMO0=\"\n}"
--- dito/server/webpack-runtime.js
+++ dito/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("91c1843f64ceccf6")
+/******/ 		__webpack_require__.h = () => ("d7c199e5c4552124")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */

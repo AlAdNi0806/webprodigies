cfb86dad8e4c427b80e9c163db0a05fe95d3049d
Alex <hetdro451@gmail.com>
2024-01-07 16:58:10 +0300

    fixed bugs-1 first page

--- dito/static/chunks/webpack.js
+++ dito/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7beff95de9abff25"; }
+/******/ 		__webpack_require__.h = function() { return "eca00e45ede374c7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
